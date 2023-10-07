2d26a1e154051c66fb3cfdfa78e7dbcba4571f01
basakerdag <basakkerdag42@gmail.com>
2023-10-08 02:08:34 +0300

    30 Ağustos Zafer Bayramı
    30 Ağustos Zafer Bayramı için yaptığım Bilgi Oyunu

---
+++ BilgiOyunu/Assets/Scripts/Butonlar.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Butonlar : MonoBehaviour
+{
+    public void YeniSahne()
+    {
+        SceneManager.LoadScene("2.Sahne");
+    }
+
+    public void YeniSahne2()
+    {
+        SceneManager.LoadScene("3.Sahne");
+    }
+
+    public void YeniSahne3()
+    {
+        SceneManager.LoadScene("4.Sahne");
+    }
+
+    public void YeniSahne4() 
+    {
+        SceneManager.LoadScene("5.Sahne");
+    }
+
+    public void YeniSahne5()
+    {
+        SceneManager.LoadScene("6.Sahne");
+    }
+    
+    public void YeniSahne6()
+    {
+        SceneManager.LoadScene("7.Sahne");
+    }
+
+    public void YeniSahne7()
+    {
+        SceneManager.LoadScene("8.Sahne");
+    }
+
+    public void YeniSahne8()
+    {
+        SceneManager.LoadScene("9.Sahne");
+    }
+
+    public void YeniSahne9()
+    {
+        SceneManager.LoadScene("10.Sahne");
+    }
+
+    public void YeniSahne10()
+    {
+        SceneManager.LoadScene("11.Sahne");
+    }
+
+    public void YeniSahne11()
+    {
+        SceneManager.LoadScene("12.Sahne");
+    }
+
+    public void YeniSahne12()
+    {
+        SceneManager.LoadScene("13.Sahne");
+    }
+
+    public void YeniSahne13()
+    {
+        SceneManager.LoadScene("14.Sahne");
+    }
+
+    public void Cikis()
+    {
+        Application.Quit();
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
